d5f714285d856deef2e61f4cdb1096e6118adf1a
tunamaguro <tsu7magu6@gmail.com>
2024-11-06 21:20:44 +0000

    tracing

--- src/main.rs
+++ src/main.rs
@@ -5,13 +5,24 @@ use tokio::net::TcpListener;
 
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
+    let _guard = apub_lite::tracing::init();
+
     bootstrap().await?;
 
     Ok(())
 }
 
 async fn bootstrap() -> anyhow::Result<()> {
-    let app = Router::new().route("/health", routing::get(health_check));
+    use tower::ServiceBuilder;
+    use tower_http::{normalize_path::NormalizePathLayer, trace::TraceLayer};
+
+    let app = Router::new()
+        .route("/health", routing::get(health_check))
+        .layer(
+            ServiceBuilder::new()
+                .layer(NormalizePathLayer::trim_trailing_slash())
+                .layer(TraceLayer::new_for_http()),
+        );
     let addr = SocketAddr::new(Ipv4Addr::LOCALHOST.into(), 8080);
     let listener = TcpListener::bind(addr).await?;
 
