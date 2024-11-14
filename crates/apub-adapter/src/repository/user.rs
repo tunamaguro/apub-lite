0e3e2e61a32e42074e3628879a0c27b98a5fd10b
tunamaguro <tsu7magu6@gmail.com>
2024-11-14 20:23:04 +0000

    chore: delete unused

--- crates/apub-adapter/src/repository/user.rs
+++ crates/apub-adapter/src/repository/user.rs
@@ -10,49 +10,6 @@ use apub_kernel::{
 
 use crate::persistence::postgres::PostgresDb;
 
-#[derive(Clone, Default)]
-pub struct InMemoryUserRepo {
-    map: Arc<Mutex<BTreeMap<String, User>>>,
-}
-
-impl InMemoryUserRepo {
-    pub fn new() -> Self {
-        let mut map = BTreeMap::new();
-
-        map.entry("alice".to_string()).or_insert(User {
-            name: "alice".to_string(),
-        });
-
-        map.entry("bob".to_string()).or_insert(User {
-            name: "bob".to_string(),
-        });
-
-        InMemoryUserRepo {
-            map: Arc::new(Mutex::new(map)),
-        }
-    }
-}
-
-#[async_trait::async_trait]
-impl UserRepository for InMemoryUserRepo {
-    #[tracing::instrument(skip(self))]
-    async fn find_by_name(&self, name: &str) -> anyhow::Result<User> {
-        let map = self.map.lock().unwrap();
-        match map.get(name).ok_or(anyhow::anyhow!("Not found")) {
-            Ok(user) => Ok(user.clone()),
-            Err(err) => Err(err),
-        }
-    }
-    #[tracing::instrument(skip(self))]
-    async fn create(&self, event: CreateUser) -> anyhow::Result<()> {
-        let mut map = self.map.lock().unwrap();
-        match map.insert(event.name.clone(), event.into()) {
-            Some(_) => Err(anyhow::anyhow!("Cannot created")),
-            None => Ok(()),
-        }
-    }
-}
-
 pub struct UserRow {
     name: String,
 }
