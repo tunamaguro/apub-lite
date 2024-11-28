82416b0390a8992507caaa3b89ca38d91e22ccb8
tunamaguro <tsu7magu6@gmail.com>
2024-11-28 13:03:38 +0000

    chore: remove migration

--- crates/apub-adapter/migrations/20241114191440_init.up.sql
+++ crates/apub-adapter/migrations/20241114191440_init.up.sql
@@ -3,3 +3,13 @@ CREATE TABLE IF NOT EXISTS users (
     id UUID PRIMARY KEY,
     name VARCHAR(255) UNIQUE NOT NULL CHECK(trim(lower(name)) = name)
 );
+
+CREATE TABLE IF NOT EXISTS user_rsa_keys(
+    user_id UUID PRIMARY KEY,
+    private_key TEXT NOT NULL CHECK(private_key <> ''),
+    public_key TEXT NOT NULL CHECK(public_key <> ''),
+
+    FOREIGN KEY (user_id) REFERENCES users(id)
+    ON UPDATE CASCADE
+    ON DELETE CASCADE
+);
